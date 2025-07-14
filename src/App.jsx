1c5cf15343f846d2ea94f45b197aa50befbedbd9
Your Name <you@example.com>
2025-07-14 16:33:06 +0200

    Add styles and structure to your app layout; integrate Bootstrap and Open Sans fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import './App.css';
 import Header from './components/Header.jsx';
 import Main from './components/Main.jsx';
 import Footer from './components/Footer.jsx';
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,21 @@
-import logo from '../assets/favicon.ico';
-
-
-
 export default function Header() {
     return (
         <header>
-            <img src={logo} alt="DC" className="logo" />
-            <nav className="nav">
-                <a href="#">Characters</a>
-                <a href="#">Comics</a>
-                <a href="#">Movies</a>
-                <a href="#">Tv</a>
-                <a href="#">Games</a>
-                <a href="#">Collectibles</a>
-                <a href="#">Video</a>
-                <a href="#">Fans</a>
-                <a href="#">News</a>
-                <a href="#">Shop</a>
-            </nav>
+            <div className="container d-flex justify-content-between align-items-center py-4">
+                <img src="/dc-logo.png" alt="DC" className="logo" />
+                <nav className="nav d-flex gap-2">
+                    <a href="#">Characters</a>
+                    <a href="#">Comics</a>
+                    <a href="#">Movies</a>
+                    <a href="#">Tv</a>
+                    <a href="#">Games</a>
+                    <a href="#">Collectibles</a>
+                    <a href="#">Video</a>
+                    <a href="#">Fans</a>
+                    <a href="#">News</a>
+                    <a href="#">Shop</a>
+                </nav>
+            </div>
         </header>
-    )
+    );
 }
